029133ae2862ae47915d76f4fba639376374d60c
tanhaipeng <tanhp@outlook.com>
2017-08-28 23:27:27 +0800

    for save

---
+++ application/controllers/Admin.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: tanhaipeng
+ * Date: 2017/8/28
+ * Time: 23:26
+ */
+class Admin extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
