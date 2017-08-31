95a318a6172652dc333047b37db77f2d07c0a46e
tanhaipeng <tanhp@outlook.com>
2017-08-31 21:38:36 +0800

    model

--- application/models/Account.php
+++ application/models/Account.php
@@ -11,10 +11,34 @@ class Account extends CI_Model
     public function __construct()
     {
         parent::__construct();
+        $this->load->database();
     }
 
     public function getAccounts()
     {
-        return array();
+        $arrAcc = array();
+        $sql = 'SELECT DISTINCT account from wcms_account';
+        $query = $this->db->query($sql);
+        if ($query) {
+            foreach ($query->result() as $row) {
+                $arrAcc[] = $row->account;
+            }
+        }
+        return $arrAcc;
+    }
+
+    public function insertAccounts()
+    {
+
+    }
+
+    public function deleteAccounts()
+    {
+
+    }
+
+    public function updateAccounts()
+    {
+
     }
 }
--- assets/javascripts/wcms.js
+++ assets/javascripts/wcms.js
@@ -1,4 +1,3 @@
 /**
  * Created by tanhaipeng on 2017/8/31.
  */
-
