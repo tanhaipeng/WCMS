cf840f44e2cefa6664ee8a87fa4ffffa4117f6ad
tanhaipeng <tanhp@outlook.com>
2017-09-14 23:51:23 +0800

    remove message and setting

--- application/views/admin/wacc.php
+++ application/views/admin/wacc.php
@@ -6,34 +6,6 @@
         Hierapolis
     </a>
     <ul class='nav navbar-nav pull-right'>
-        <li class='dropdown'>
-            <a class='dropdown-toggle' data-toggle='dropdown' href='#'>
-                <i class='icon-envelope'></i>
-                Messages
-                <span class='badge'>5</span>
-                <b class='caret'></b>
-            </a>
-            <ul class='dropdown-menu'>
-                <li>
-                    <a href='#'>New message</a>
-                </li>
-                <li>
-                    <a href='#'>Inbox</a>
-                </li>
-                <li>
-                    <a href='#'>Out box</a>
-                </li>
-                <li>
-                    <a href='#'>Trash</a>
-                </li>
-            </ul>
-        </li>
-        <li>
-            <a href='#'>
-                <i class='icon-cog'></i>
-                Settings
-            </a>
-        </li>
         <li class='dropdown user'>
             <a class='dropdown-toggle' data-toggle='dropdown' href='#'>
                 <i class='icon-user'></i>
