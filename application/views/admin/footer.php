211399614c4b90d92a810de8f4697030042e53cd
tanhaipeng <tanhp@outlook.com>
2017-09-15 00:07:54 +0800

    delete ? char

--- application/views/admin/footer.php
+++ application/views/admin/footer.php
@@ -3,6 +3,6 @@
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js" type="text/javascript"></script>
 <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js" type="text/javascript"></script>
 <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js" type="text/javascript"></script>
-<script src="<?= $assets . "/javascripts/application.js?" ?>" type="text/javascript"></script>
+<script src="<?= $assets . "/javascripts/application.js" ?>" type="text/javascript"></script>
 <script src="<?= $assets . "/javascripts/wcms.js?v=" . $ts ?>" type="text/javascript"></script>
 </html >
